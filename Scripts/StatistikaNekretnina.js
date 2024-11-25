ef4f442b94486714149961922cc8a25a2e9fd003
Ismar Muslić <ismar.muslic@outlook.com>
2024-11-25 23:14:31 +0100

    Added globals + init() function

---
+++ Scripts/StatistikaNekretnina.js
@@ -0,0 +1,9 @@
+const listaKorisnika = Array.from(nekretnine.listaKorisnika);
+const listaNekretnina = Array.from(nekretnine.listaNekretnina);
+
+var spisakNekretnina = SpisakNekretnina();
+
+let init = function(listaNekretnina, listaKorisnika) { 
+    spisakNekretnina.init(listaNekretnina, listaKorisnika);
+}
+

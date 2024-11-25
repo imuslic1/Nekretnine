a2bbd43c6a11ddb329d1af641581581246cb55c7
Ismar Muslić <ismar.muslic@outlook.com>
2024-11-25 23:15:15 +0100

    Added prosjecnaKvadratura() method

--- Scripts/StatistikaNekretnina.js
+++ Scripts/StatistikaNekretnina.js
@@ -7,3 +7,14 @@ let init = function(listaNekretnina, listaKorisnika) {
     spisakNekretnina.init(listaNekretnina, listaKorisnika);
 }
 
+let prosjecnaKvadratura = function(kriterij) {
+    let listaNekretninaPoKriteriju = Array.from(SpisakNekretnina.filtrirajNekretnine(kriterij));
+    let sumaKvadratura = 0;
+
+    listaNekretninaPoKriteriju.forEach(element => {
+        let kvadratura = element.kvadratura;
+        sumaKvadratura += kvadratura;
+    });
+
+    return sumaKvadratura / listaNekretninaPoKriteriju.length;
+}

1d25833c62a9fce4825038ca07af204d249d2697
Ismar Muslić <ismar.muslic@outlook.com>
2024-11-25 23:16:40 +0100

    Added outlier() + helper function to calculate average of any given property

--- Scripts/StatistikaNekretnina.js
+++ Scripts/StatistikaNekretnina.js
@@ -18,3 +18,32 @@ let prosjecnaKvadratura = function(kriterij) {
 
     return sumaKvadratura / listaNekretninaPoKriteriju.length;
 }
+
+let prosjecnaVrijednostSvojstva = function(kriterij, nazivSvojstva) {
+    let listaNekretninaPoKriteriju = Array.from(SpisakNekretnina.filtrirajNekretnine(kriterij));
+    let sumaSvojstva = 0;
+
+    listaNekretninaPoKriteriju.forEach(element => {
+        let svojstvo = listaNekretninaPoKriteriju[nazivSvojstva];
+        sumaSvojstva += svojstvo;
+    });
+
+    return sumaSvojstva / listaNekretninaPoKriteriju.length;
+}
+
+let outlier = function(kriterij, nazivSvojstva) {
+    let listaNekretninaPoKriteriju = Array.from(SpisakNekretnina.filtrirajNekretnine(kriterij));
+    let prosjek = prosjecnaVrijednostSvojstva(kriterij, nazivSvojstva);
+    let indexMaxOdstupanja = 0;
+    let maxOdstupanje = 0;
+
+    for(let i=0; i<listaNekretninaPoKriteriju.length; ++i) {
+        let odstupanje = Math.abs(listaNekretninaPoKriteriju[i][nazivSvojstva] - prosjek);
+        if(odstupanje > maxOdstupanje) {
+            maxOdstupanje = odstupanje;
+            indexMaxOdstupanja = i;
+        }
+    }
+
+    return listaNekretninaPoKriteriju[indexMaxOdstupanja];
+}
